08d2c65d808a29378b8a6c67b9d96dd89004d87c
Takeru SHIGEMORI <sgtakeru@gmail.com>
2014-03-05 12:09:56 +0900

    Add  vim, vi setting

--- home/.zsh/custom/020_alias.zsh
+++ home/.zsh/custom/020_alias.zsh
@@ -42,6 +42,8 @@ export LESS='-R'
 export LESSOPEN='| /usr/share/source-highlight/src-hilite-lesspipe.sh %s'
 
 
+alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -u $HOME/.vimrc "$@"'
+alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim -u $HOME/.vimrc "$@"'
 
 # =====================================================
 # Define functions
