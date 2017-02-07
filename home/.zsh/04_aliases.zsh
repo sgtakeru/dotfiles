40aa2c69faa662be5e0946f50572756da539927b
Takeru SHIGEMORI <shigemori@haw.co.jp>
2017-02-07 11:36:51 +0900

    Add Hammerspoon restart command
    Hammerspoonのプロセスがあるのにキーバインドが無効になることがあるので
    再起動コマンドを追加する

--- home/.zsh/04_aliases.zsh
+++ home/.zsh/04_aliases.zsh
@@ -84,3 +84,7 @@ fi
 
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+
+# restart hammerspoon process
+alias restartHammer='pkill Hammerspoon && open -a /Applications/Hammerspoon.app'
