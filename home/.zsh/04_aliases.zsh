65e75467ad1a78ee12d3bdff6ee7b4d694727bfa
Takeru SHIGEMORI <shigemori@haw.co.jp>
2019-10-26 17:28:06 +0900

    Add delte local branch alias.

--- home/.zsh/04_aliases.zsh
+++ home/.zsh/04_aliases.zsh
@@ -10,6 +10,7 @@ alias tree='tree -C'
 if has 'git'; then
     alias gst='git status'
     alias g="git"
+    alias git-delete-local-branches="git branch --merged | grep -vE '^\*|master$|develop$' | xargs -p -I % git branch -d %"
 fi
 
 
@@ -87,6 +88,3 @@ fi
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
-
-# restart hammerspoon process
-alias restartHammer='pkill Hammerspoon && open -a /Applications/Hammerspoon.app'
