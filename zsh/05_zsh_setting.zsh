5a776e6d11e565a9e07ef9a626fe768e0df26680
Takeru Shigemori <shigemori@haw.co.jp>
2021-01-23 17:00:33 +0900

    Manage .zsh_hisotry with dropbox

--- zsh/05_zsh_setting.zsh
+++ zsh/05_zsh_setting.zsh
@@ -6,7 +6,7 @@
 
 autoload -Uz compinit && compinit
 
-HISTFILE=$ZDOTDIR/.zsh_history
+HISTFILE=$HOME/dotfiles/zsh/.zsh_history
 HISTSIZE=10000000
 SAVEHIST=10000000
 REPORTTIME=3                # 3秒以上かかった処理は詳細表示
